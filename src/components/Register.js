d92cbaa873d23b66199206fb2f88536d6852cbb2
Anum Iqbal <ianum0164@gmail.com>
2024-09-22 03:45:59 +0500

    feat: Integrate React Hook Form to manage form states

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,26 +1,41 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    alert("Registration successful!");
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label>Username</label>
-        <input type="text" name="username" />
+        <input type="text" name="username" {...register("username")} />
       </div>
 
       <div>
         <label>Email</label>
-        <input type="email" name="email" />
+        <input type="email" name="email" {...register("email")} />
       </div>
 
       <div>
         <label>Password</label>
-        <input type="password" name="password" />
+        <input type="password" name="password" {...register("password")} />
       </div>
 
       <div>
         <label>Confirm Password</label>
-        <input type="password" name="confirmPassword" />
+        <input
+          type="password"
+          name="confirmPassword"
+          {...register("confirmPassword")}
+        />
       </div>
 
       <button type="submit">Submit</button>
